09b887c6124c8bc925a3bd71f74b3e9eb32e5513
MaxMalts <max-kurt@bk.ru>
2020-12-15 13:15:08 +0300

    Contest 9 fixed

--- Contest_9/tech09-1/tech09-1.c
+++ Contest_9/tech09-1/tech09-1.c
@@ -1,128 +1,167 @@
 #define _XOPEN_SOURCE 700
+
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <linux/limits.h>
+#include <netinet/in.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
- 
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <sys/types.h>
- 
-#include <netinet/in.h>
- 
-const size_t BUF_LEN = 4096;
-char waiting = 0;
-sig_atomic_t IS_RUNNING = 1;
-void sigterm_handler(int sig_num)
+#include <unistd.h>
+
+int needToFinish = 0;
+
+void Finish(int signal)
 {
-    IS_RUNNING = 0;
-    if (waiting)
-        exit(0);
+    needToFinish = 1;
 }
- 
-void make_sigaction(struct sigaction* sigact, void (*func)(int), int flags)
+
+int AddSigactions()
 {
-    memset(sigact, 0, sizeof(struct sigaction));
-    sigact->sa_flags = flags;
-    sigact->sa_handler = func;
+    struct sigaction sigterm = {};
+    sigterm.sa_flags = SA_RESTART;
+    sigterm.sa_handler = Finish;
+
+    if (sigaction(SIGINT, &sigterm, NULL) == -1 ||
+        sigaction(SIGTERM, &sigterm, NULL) == -1) {
+
+        return -1;
+    }
+
+    return 0;
 }
- 
-int main(int argc, char* argv[])
+
+int InitServer(const char* ip_str, int port)
 {
-    printf("%d", getpid());
- 
-    if (argc != 3)
-        return 1;
- 
-    struct sigaction sigterm;
-    make_sigaction(&sigterm, sigterm_handler, SA_RESTART);
-    if (sigaction(SIGTERM, &sigterm, NULL) == -1)
-        return 1;
-    if (sigaction(SIGINT, &sigterm, NULL) == -1)
-        return 1;
- 
-    char ip_str[] = "127.0.0.1";
-    unsigned int ip = 0;
-    for (int i = 0; i < sizeof(ip); ++i) {
-        *((char*)&ip + i) = atoi(strtok(i == 0 ? ip_str : NULL, "."));
+    assert(ip_str != NULL);
+    assert(port > 0);
+
+    struct sockaddr_in sockAddr;
+    sockAddr.sin_family = AF_INET;
+    sockAddr.sin_addr.s_addr = inet_addr(ip_str);
+    sockAddr.sin_port = htons(port);
+
+    int resSock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (bind(resSock, (struct sockaddr*)&sockAddr, sizeof(sockAddr)) == -1) {
+        return -1;
     }
- 
-    struct in_addr ip_struct;
-    ip_struct.s_addr = 0;
- 
-    struct sockaddr_in sockaddr;
-    sockaddr.sin_family = AF_INET;
-    sockaddr.sin_port = htons(atoi(argv[1]));
-    ;
-    sockaddr.sin_addr = ip_struct;
- 
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
- 
-    if (bind(socket_fd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) == -1)
-        return 1;
- 
-    if (listen(socket_fd, SOMAXCONN) == -1)
-        return 1;
- 
-    char buff[BUF_LEN];
-    char junk[BUF_LEN];
- 
-    while (IS_RUNNING) {
-        waiting = 1;
-        int client_fd = accept(socket_fd, 0, 0);
-        if (client_fd == -1)
-            return 1;
-        waiting = 0;
- 
-        int bytes = 0;
-        while (bytes < BUF_LEN &&
-               (bytes = read(client_fd + bytes, buff, BUF_LEN - 1)) > 0)
-            if (strstr(buff, "\r\n\r\n") != 0)
-                break;
- 
-        char path[BUF_LEN];
-        //if (*argv[2] != '/')
-        strncpy(path, argv[2], BUF_LEN);
-        //else
-        //strncpy(path, argv[2] + 1, BUF_LEN);
- 
-        int len = strlen(path);
-        if (path[len - 1] != '/' && len < BUF_LEN) {
-            path[len] = '/';
-            ++len;
-        }
-        char* filename = buff + 4;
- 
-        char* filename_end = strstr(filename, " HTTP/1.1");
-        *filename_end = 0;
- 
-        strcat(path, filename);
-        printf("%s\n", path);
-        struct stat file_stat;
-        if (lstat(path, &file_stat) == -1) {
-            dprintf(client_fd, "HTTP/1.1 404 Not Found\r\n");
-        } else if (access(path, R_OK) == -1) {
-            dprintf(client_fd, "HTTP/1.1 403 Forbidden\r\n");
-        } else {
-            char outbuff[BUF_LEN * 16];
-            snprintf(
-                outbuff,
-                BUF_LEN * 16 - 1,
-                "HTTP/1.1 200 OK\r\nContent-Length: %ld\r\n\r\n",
-                file_stat.st_size);
-            char* start_write = outbuff + strlen(outbuff);
- 
-            int fd = open(path, O_RDONLY);
-            read(fd, start_write, outbuff + BUF_LEN * 16 - start_write);
- 
-            write(client_fd, outbuff, strlen(outbuff));
-        }
- 
-        close(client_fd);
-        shutdown(client_fd, SHUT_RDWR);
+
+    if (listen(resSock, SOMAXCONN) == -1) {
+        return -1;
     }
-    close(socket_fd);
-    shutdown(socket_fd, SHUT_RDWR);
+
+    return resSock;
+}
+
+void HandleClient(int clientSock, const char* dataDir)
+{
+    assert(clientSock > 0);
+
+    const int bufLen = 20000;
+    char buf[bufLen];
+    char fullPath[PATH_MAX] = "";
+
+    int bytesRead = read(clientSock, buf, bufLen - 1);
+    buf[bytesRead] = '\0';
+
+    char garbage[bufLen];
+    garbage[0] = '\0';
+    while (strstr(garbage, "\r\n\r\n") != 0) {
+        bytesRead = read(clientSock, garbage, sizeof(garbage) - 1);
+        garbage[bytesRead] = '\0';
+    }
+
+    char* reqPath = buf + 4;
+    *(strstr(reqPath, " HTTP/1.1")) = '\0';
+
+    strncpy(fullPath, dataDir, PATH_MAX);
+    strcat(fullPath, reqPath);
+
+    struct stat fStats;
+    if (lstat(fullPath, &fStats) == -1) {
+        char writeStr[] = "HTTP/1.1 404 Not Found\r\n";
+        write(clientSock, writeStr, sizeof(writeStr) - 1);
+
+    } else if (access(fullPath, R_OK) == -1) {
+        char writeStr[] = "HTTP/1.1 403 Forbidden\r\n";
+        write(clientSock, writeStr, sizeof(writeStr) - 1);
+
+    } else {
+        lstat(fullPath, &fStats);
+        snprintf(
+            buf,
+            bufLen - 1,
+            "HTTP/1.1 200 OK\r\nContent-Length: %ld\r\n\r\n",
+            fStats.st_size);
+        int headerLen = strlen(buf);
+
+        int file = open(fullPath, O_RDONLY);
+        int bytesRead = read(file, buf + headerLen, bufLen - headerLen - 1);
+        assert(bytesRead < bufLen - headerLen - 1);
+        close(file);
+
+        write(clientSock, buf, headerLen + bytesRead);
+    }
+
+    shutdown(clientSock, SHUT_RDWR);
+    close(clientSock);
+}
+
+int RunServer(int listenSock, const char* dataDir)
+{
+    printf("%s\n", dataDir);
+    assert(listenSock > 0);
+
+    int listenSockFlags =
+        fcntl(listenSock, F_GETFL); // For needToFinish to work
+    listenSockFlags |= O_NONBLOCK;
+    fcntl(listenSock, F_SETFL, listenSockFlags);
+
+    while (1) {
+        int clientSock = 0;
+        do {
+            if (needToFinish) {
+                return 0;
+            }
+            clientSock = accept(listenSock, 0, 0);
+        } while (-1 == clientSock);
+
+        HandleClient(clientSock, dataDir);
+    }
+}
+
+int main(const int argc, const char* argv[])
+{
+    if (argc != 3) {
+        return -1;
+    }
+
+    if (AddSigactions() == -1) {
+        return -1;
+    }
+
+    int listenSock = InitServer("127.0.0.1", atoi(argv[1]));
+    if (-1 == listenSock) {
+        return -1;
+    }
+
+    char dataDir[strlen(argv[2]) + 2];
+    strncpy(dataDir, argv[2], sizeof(dataDir) - 1);
+    int dataDirLen = strlen(dataDir);
+    if (dataDir[dataDirLen - 1] != '/') {
+        dataDir[dataDirLen] = '/';
+        dataDir[dataDirLen + 1] = '\0';
+    }
+    RunServer(listenSock, dataDir);
+
+    shutdown(listenSock, SHUT_RDWR);
+    close(listenSock);
+    return 0;
 }
